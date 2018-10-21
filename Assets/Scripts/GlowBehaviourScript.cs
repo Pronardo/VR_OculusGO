eb3c6cc4c159167b268c0d06cf8b2527c679f1f0
Pronardo <projetekt@outlook.de>
2018-10-21 19:08:33 +0200

    Gaze Test
    First sucessful gaze test on oculus go

---
+++ Assets/Scripts/GlowBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowBehaviourScript : MonoBehaviour
+{
+
+    public Material HighlightedMaterial;
+    public Material DefaultMaterial;
+    private Renderer sRenderer;
+
+    void Start()
+    {
+        this.sRenderer = GetComponent<Renderer>();
+        DefaultRendering();
+    }
+
+    public void HighlightRendering()
+    {
+        sRenderer.material = HighlightedMaterial;
+        Debug.Log("GlowBehaviour: SetMaterial - Highlight");
+    }
+
+    public void DefaultRendering()
+    {
+        sRenderer.material = DefaultMaterial;
+        Debug.Log("GlowBehaviour: SetMaterial - Default");
+    }
+
+}
